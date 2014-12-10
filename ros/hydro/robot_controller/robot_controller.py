a169b49866087c883e20e5170ed7b1ff0a535918
Eric Schneider <eric.schneider@liverpool.ac.uk>
2014-12-10 19:23:33 +0000

    Added several launch files.

--- ros/hydro/robot_controller/robot_controller.py
+++ ros/hydro/robot_controller/robot_controller.py
@@ -2,6 +2,7 @@
 
 # Standard modules
 import sys
+import time
 import uuid
 
 # ROS modules
@@ -24,16 +25,25 @@ class RobotController:
 
     def __init__(self):
 
+#        time.sleep(5)
+        
         argv = rospy.myargv(argv=sys.argv[1:])
         print "arguments: {0}".format(argv)
 
         # Our robot name is the first arg to our program 
-        robot_name = None
+        self.robot_name = None
         if not argv:
             # a random ID with dashes and underscores removed
-            robot_name = str(uuid.uuid1()).replace('-','').replace('_','')
+            self.robot_name = str(uuid.uuid1()).replace('-','').replace('_','')
         else:
-            robot_name = argv[0]
+            self.robot_name = argv[0]
+
+        print "Robot name: {0}".format(self.robot_name)
+
+        self.goal_x = float(argv[1])
+        self.goal_y = float(argv[2])
+
+        print "Goal: ({0}, {1})".format(self.goal_x, self.goal_y)
 
         # What messages do we want to publish?
         # - Send bids to auctioneer
@@ -42,7 +52,7 @@ class RobotController:
 #        self.pub = rospy.Publisher('robot_status', String, queue_size=10)
 
         # Initialize our node
-        node_name = "rc_{0}".format(robot_name)
+        node_name = "rc_{0}".format(self.robot_name)
 #        rospy.loginfo("Starting node '{0}'...".format(node_name))
         print("Starting node '{0}'...".format(node_name))
         rospy.init_node(node_name)
@@ -54,8 +64,8 @@ class RobotController:
         self.init_publishers()
 
         # actionlib client; used to send goals to the navigation stack
-#        ac_name = "ac_{0}".format(robot_name)
-        ac_name = "move_base"
+#        ac_name = "ac_{0}".format(self.robot_name)
+        ac_name = "/{0}/move_base".format(self.robot_name)
         print "Starting actionlib client '{0}'".format(ac_name)
         self.aclient = actionlib.SimpleActionClient(ac_name,
                                                     move_base_msgs.msg.MoveBaseAction)
@@ -66,8 +76,9 @@ class RobotController:
         print "{0} connected.".format(ac_name)
 
         # Set up state machine
-#        self._state = RCState.IDLE
-        self._state = RCState.INIT_POSE
+        #        self._state = RCState.IDLE
+        #self._state = RCState.INIT_POSE
+        self._state = RCState.SEND_GOAL
 
     def set_gound_truth_pose(self, data):
 #        print 'Setting ground truth pose...'
@@ -79,13 +90,13 @@ class RobotController:
 
         # 'base_pose_ground_truth'
         print '  base_pose_ground_truth'
-        self.ground_truth_sub = rospy.Subscriber('base_pose_ground_truth',
+        self.ground_truth_sub = rospy.Subscriber("/{0}/base_pose_ground_truth".format(self.robot_name),
                                                  nav_msgs.msg.Odometry,
                                                  self.set_gound_truth_pose)
 
     def init_publishers(self):
         # 'initialpose'
-        self.initial_pose_pub = rospy.Publisher('initialpose',
+        self.initial_pose_pub = rospy.Publisher("/{0}/initialpose".format(self.robot_name),
                                                 geometry_msgs.msg.PoseWithCovarianceStamped)
 
     def send_initial_pose(self):
@@ -109,12 +120,12 @@ class RobotController:
     def send_goal(self):
         goal = move_base_msgs.msg.MoveBaseGoal()
 
-        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.frame_id = 'map'
         goal.target_pose.header.stamp = rospy.Time.now()
 
-        goal.target_pose.pose.position.x = 40.0
-        goal.target_pose.pose.position.y = 20.0
-        goal.target_pose.pose.orientation.w = 0.9
+        goal.target_pose.pose.position.x = float(self.goal_x)
+        goal.target_pose.pose.position.y = float(self.goal_y)
+        goal.target_pose.pose.orientation.w = 1.0
 
         print "Sending goal: {0}".format(goal)
         self.aclient.send_goal(goal)
@@ -130,7 +141,8 @@ class RobotController:
         while not rospy.is_shutdown():
             # Do something
             if self._state == RCState.IDLE:
-                print "Idle..."
+                #print "Idle..."
+                pass
             elif self._state == RCState.INIT_POSE:
                 self.send_initial_pose()
             elif self._state == RCState.SEND_GOAL:
