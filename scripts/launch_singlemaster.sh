57687f0fef174278b8aff91b956bc33891503caf
Eric Schneider <eric.schneider@liverpool.ac.uk>
2015-04-16 19:06:06 +0100

    Changed Stage simulation step to 50ms

---
+++ scripts/launch_singlemaster.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+RUN_COUNT=30
+
+#for map in brooklyn # smartlab
+for map in smartlab
+do
+    #for task_file in brooklyn_tasks_A.txt brooklyn_tasks_C.txt brooklyn_tasks_E.txt
+    for task_file in tasks_A.txt
+    do
+	for mechanism in OSI SSI PSI RR
+	do
+	    for start_config in distributed clustered
+	    do
+		for run in `seq 1 ${RUN_COUNT}`
+		do
+		    ./launch_experiment_singlemaster.py ${mechanism} ${map} ${start_config} ${task_file}
+
+		done # end "run"
+
+	    done # end "start_config"
+
+	done # end "mechanism"
+
+    done # end "task_file"
+
+done # end "map"
