4bdbdff899125da508d707b9f244ae00531530db
Eric Schneider <eric.schneider@liverpool.ac.uk>
2015-10-19 18:20:27 +0100

    parse_stats_csv.py updated to work with the latest bag formats.

---
+++ scripts/analysis/check_complete.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+
+import argparse
+from collections import defaultdict
+import glob
+import os
+from os import listdir
+import os.path
+from os.path import isdir, isfile, join
+import re
+import rosbag
+import rospkg
+import shutil
+import sys
+import yaml
+
+import mrta.msg
+
+def check_complete(bag_paths):
+
+    dt_re = re.compile('(.*)\.bag')
+
+    for i,bag_path in enumerate(bag_paths):
+#        print("Reading {0}".format(bag_path))
+
+        bag = None
+        try:
+            bag = rosbag.Bag(bag_path)
+        except:
+            print("Couldn't open {0} for reading!".format(bag_path))
+            continue
+
+        bag_filename = os.path.basename(bag_path)
+        bag_dirname = os.path.dirname(bag_path)
+
+        # Create the 'failed' directory if it doesn't exist
+        failed_dirname = os.path.join(bag_dirname, 'FAILED')
+        if not os.path.isdir(failed_dirname):
+            print('...Creating {0}'.format(failed_dirname))
+            os.mkdir(failed_dirname)
+
+        (map, start_config, mechanism, task_file, remainder) = bag_filename.split('__')
+
+        # Date/time
+        dt_match = dt_re.search(remainder)
+        dt_string = dt_match.group(1) # 'DATETIME'
+
+        run_msgs = defaultdict(list)
+
+        for topic,msg,msg_time in bag.read_messages():
+            run_msgs[topic].append(msg)
+
+        bag.close()
+
+        exp_msgs = {}
+        for msg in run_msgs['/experiment']:
+#            print msg
+            exp_msgs[msg.event] = msg
+
+
+        print('{0}-{1} ({2}):'.format(start_config, mechanism, dt_string)),
+
+        if mrta.msg.ExperimentEvent.END_EXPERIMENT in exp_msgs:
+            print('  COMPLETE!')
+        else:
+            print('  FAILED!')
+            # move the file to the 'failed' directory
+            shutil.move(bag_path, os.path.join(failed_dirname, bag_filename))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Check if the experiments for a given set of bag paths actually completed.')
+
+    parser.add_argument('bag_file',
+                        nargs='+',
+                        help='Path to the bag file of the experiment to plot.')
+
+    args = parser.parse_args()
+
+    bag_files = args.bag_file
+
+#    pp.pprint(bag_files)
+
+    # Make one, flat list of paths to log files
+    bag_paths = []
+
+    for path_arg in bag_files:
+        bag_paths.extend(glob.glob(path_arg))
+
+#    print("bag paths: ")
+#    pp.pprint(bag_paths)
+
+    check_complete(bag_paths)
--- scripts/analysis/parse_stats_csv.py
+++ scripts/analysis/parse_stats_csv.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python
 
+import argparse
 from collections import defaultdict
 import csv
 import glob
 import math
 import os
+import pprint
 import re
 import rosbag
 import sys
 
-CSV_FILENAME = 'stats.csv'
+import mrta
+import mrta.msg
+
+DEFAULT_CSV_FILENAME = 'stats.csv'
 
 field_names = [
     'BAG_FILENAME',
@@ -22,25 +27,31 @@ field_names = [
 #    'ATTEMPT_NUM',
     'TOTAL_RUN_TIME',
     'DELIBERATION_TIME',
-    'EXECUTION_TIME',
+    'EXECUTION_PHASE_TIME',
+    'TOTAL_MOVEMENT_TIME',
+    'TOTAL_EXECUTION_TIME',
+    'TOTAL_WAITING_TIME',
     'TOTAL_IDLE_TIME',
     'TOTAL_DELAY_TIME',
     'TOTAL_DISTANCE',
     'TOTAL_COLLISIONS',
-    'ANNOUNCE_MSGS',
-    'ANNOUNCE_MSG_TASKS',
-    'BID_MSGS',
-    'ALLOC_MSGS_BYTES',
+    'NUM_ANNOUNCE_MSGS',
+    'NUM_ANNOUNCE_TASKS',
+    'NUM_BID_MSGS',
+    'ALLOC_MSG_BYTES',
     'ROBOT1_DISTANCE',
-    'ROBOT1_TRAVEL_TIME',
+    'ROBOT1_MOVEMENT_TIME',
+    'ROBOT1_WAITING_TIME',
     'ROBOT1_IDLE_TIME',
     'ROBOT1_DELAY_TIME',
     'ROBOT2_DISTANCE',
-    'ROBOT2_TRAVEL_TIME',
+    'ROBOT2_MOVEMENT_TIME',
+    'ROBOT2_WAITING_TIME',
     'ROBOT2_IDLE_TIME',
     'ROBOT2_DELAY_TIME',
     'ROBOT3_DISTANCE',
-    'ROBOT3_TRAVEL_TIME',
+    'ROBOT3_MOVEMENT_TIME',
+    'ROBOT3_WAITING_TIME',
     'ROBOT3_IDLE_TIME',
     'ROBOT3_DELAY_TIME',
 ]
@@ -49,36 +60,69 @@ ROBOT_NAMES = [ 'robot_1',
                 'robot_2',
                 'robot_3' ]
 
+pp = pprint.PrettyPrinter(indent=4)
+
 class Robot(object):
     def __init__(self):
         self.distance = 0.0
-        self.travel_time = 0.0
+        self.movement_time = 0.0
         self.idle_time = 0.0
         self.delay_time = 0.0
 
         self.collisions = 0
 
+        self.exec_phase_begin_stamp = None
+        self.exec_phase_end_stamp = None
+
         # Times at which this robot paused during execution of tasks. Keys are task ids
         # and values are timestamps
         self.pause_times = defaultdict(int)
 
-def usage():
-    print 'Usage: ' + sys.argv[0] + ': <path to .bag file(s)>'
+def count_interval_times(start_event, end_event, messages):
+    """
+    Count the time in all [start_event,end_event] intervals and return the sum.
+    """
+    interval_times = 0.0
 
-def main(argv):
+    last_start_stamp = None
 
-    if len(argv) < 1:
-        usage()
-        sys.exit(1)
+    # The name of a message's 'event' field depends on its class.
+    attr_name = None
+    sample_msg = messages[0]
+#    if isinstance(sample_msg, mrta.msg.ExperimentEvent):
+    if sample_msg.__class__.__name__ == '_mrta__ExperimentEvent':
+        attr_name = 'event'
+#    elif isinstance(sample_msg, mrta.msg.TaskStatus):
+    elif sample_msg.__class__.__name__ == '_mrta__TaskStatus':
+        attr_name = 'status'
+    else:
+        # Should actually raise an error here
+        print('Unrecognized message class: {0}'.format(sample_msg.__class__.__name__))
+        return interval_times
 
-    # Make one, flat list of paths to log files
-    bag_paths = []
+    for message in messages:
+        event = message.__getattribute__(attr_name)
+        if  event == start_event:
+            last_start_stamp = message.header.stamp
+            continue
+        elif event == end_event:
+            if not last_start_stamp:
+                print("count_interval_time(): {0} not preceded by a {1}!".format(end_event, start_event))
+                continue
+            else:
+                interval_time = message.header.stamp - last_start_stamp
+                interval_secs = interval_time.secs + (interval_time.nsecs/1000000000.)
+                print("{0}--{1}=={2}".format(start_event, end_event, interval_secs))
+                interval_times += interval_secs
+                last_start_stamp = None
 
-    for path_arg in argv:
-        bag_paths.extend(glob.glob(path_arg))
+    print("total: {0}".format(interval_times))
+    return interval_times
+
+def parse_stats(bag_paths, output):
 
     # Open the CSV file for writing
-    csv_file = csv.writer(open(CSV_FILENAME, 'wb'))
+    csv_file = csv.writer(open(output, 'wb'))
     csv_file.writerow(field_names)
 
     dt_re = re.compile('(.*)\.bag')
@@ -92,52 +136,69 @@ def main(argv):
         except:
             print("Couldn't open {0} for reading!".format(bag_path))
             continue
-        
-        row_fields = []
+
+        row_fields = {}
         bag_filename = os.path.basename(bag_path)
-        row_fields.append(bag_filename)
+        row_fields['BAG_FILENAME'] = bag_filename
 
         (map, start_config, mechanism, task_file, remainder) = bag_filename.split('__')
-        
-        dt_match = dt_re.search(remainder)
 
         # Date/time
-        row_fields.append(dt_match.group(1)) # 'DATETIME'
+        dt_match = dt_re.search(remainder)
+        row_fields['DATETIME'] = dt_match.group(1)
 
         # 'START_CONFIG', 'MECHANSIM', 'TASK_FILE'
-        row_fields.extend([map, start_config, mechanism, task_file])
+#        row_fields.extend([map, start_config, mechanism, task_file])
+        row_fields['MAP'] = map
+        row_fields['START_CONFIG'] = start_config
+        row_fields['MECHANISM'] = mechanism
+        row_fields['TASK_FILE'] = task_file
 
         run_msgs = defaultdict(list)
 
         for topic,msg,msg_time in bag.read_messages():
             run_msgs[topic].append(msg)
 
-        exp_msgs = {}
+        exp_msgs = []
+        experiment_finished = False
         for msg in run_msgs['/experiment']:
-            exp_msgs[msg.event] = msg
+            if msg.event == mrta.msg.ExperimentEvent.END_EXPERIMENT:
+                experiment_finished = True
+            exp_msgs.append(msg)
 
-        if 'END_EXPERIMENT' not in exp_msgs:
+        #if 'END_EXPERIMENT' not in exp_msgs:
+        if not experiment_finished:
             print("Experiment timed out! Skipping...")
             continue
 
-        # Total run time
-        total_diff = (exp_msgs['END_EXPERIMENT'].header.stamp - 
-                      exp_msgs['BEGIN_EXPERIMENT'].header.stamp)
-        total_run_time = (total_diff.secs + total_diff.nsecs/1000000000.)
-        row_fields.append(total_run_time) # 'TOTAL_RUN_TIME'
+        print('TOTAL_RUN_TIME:')
+        total_run_time = count_interval_times(mrta.msg.ExperimentEvent.BEGIN_EXPERIMENT,
+                                              mrta.msg.ExperimentEvent.END_EXPERIMENT,
+                                              exp_msgs)
+        row_fields['TOTAL_RUN_TIME'] = total_run_time # 'TOTAL_RUN_TIME'
 
-        # Deliberation time
-        delib_diff = (exp_msgs['END_ALLOCATION'].header.stamp - 
-                      exp_msgs['BEGIN_ALLOCATION'].header.stamp)
-        delib_time = (delib_diff.secs + delib_diff.nsecs/1000000000.)
-        row_fields.append(delib_time) # 'DELIBERATION_TIME'
+        print('DELIBERATION_TIME:')
+        delib_time = count_interval_times(mrta.msg.ExperimentEvent.BEGIN_ALLOCATION,
+                                          mrta.msg.ExperimentEvent.END_ALLOCATION,
+                                          exp_msgs)
+        row_fields['DELIBERATION_TIME'] = delib_time # 'DELIBERATION_TIME'
 
-        # Execution time
-        exec_diff = (exp_msgs['END_EXECUTION'].header.stamp - 
-                      exp_msgs['BEGIN_EXECUTION'].header.stamp)
-        exec_time = (exec_diff.secs + exec_diff.nsecs/1000000000.)
-        row_fields.append(exec_time) # 'EXECUTION_TIME'        
+        print('EXECUTION_PHASE_TIME:')
+        exec_phase_time = count_interval_times(mrta.msg.ExperimentEvent.BEGIN_EXECUTION,
+                                               mrta.msg.ExperimentEvent.END_EXECUTION,
+                                               exp_msgs)
+        row_fields['EXECUTION_PHASE_TIME'] = exec_phase_time # 'EXECUTION_PHASE_TIME'
 
+
+        # The timestamp of the last 'END_EXECUTION' message
+        exp_end_execution_stamp = None
+        for msg in exp_msgs:
+            if msg.event == mrta.msg.ExperimentEvent.END_EXECUTION:
+                exp_end_execution_stamp = msg.header.stamp
+
+        total_movement_time = 0.0
+        total_execution_time = 0.0
+        total_waiting_time = 0.0
         total_idle_time = 0.0
         total_delay_time = 0.0
         total_distance = 0.0
@@ -145,6 +206,7 @@ def main(argv):
 
         robots = {}
 
+        # per-robot stats
         for r_name in ROBOT_NAMES:
             robot = Robot()
             robots[r_name] = robot
@@ -158,111 +220,165 @@ def main(argv):
                 if amcl_pose.position.x == 0 and amcl_pose.position.y == 0:
                     continue
 
+                #print("last_pose: {0}".format(last_pose))
+                #print("amcl_pose: {0}".format(last_pose))
+
                 if last_pose is None:
                     last_pose = amcl_pose
                     continue
 
                 pos_delta = math.hypot(amcl_pose.position.x - last_pose.position.x,
                                        amcl_pose.position.y - last_pose.position.y)
+#                print "{0} pos_delta: from ({1},{2}) to ({3},{4}) is {5}".format(r_name,
+#                                                                                 amcl_pose.position.x,
+#                                                                                 amcl_pose.position.y,
+#                                                                                 last_pose.position.x,
+#                                                                                 last_pose.position.y,
+#                                                                                 pos_delta)
                 robot.distance += pos_delta
+                last_pose = amcl_pose
 
-            #print "{0} distance: {1}".format(r_name, distance)
-            total_distance += robot.distance
-            
-            # Travel time
-            robot.travel_begin_time = None
-            robot.travel_end_time = None
-            for status_msg in run_msgs['/tasks/status']:
+#            print "{0} distance: {1}".format(r_name, robot.distance)
 
-                if status_msg.robot_id != r_name:
-                    continue
+            r_status_msgs = [m for m in run_msgs['/tasks/status'] if m.robot_id == r_name]
 
-                if status_msg.status == 'BEGIN' and robot.travel_begin_time is None:
-                    robot.travel_begin_time = status_msg.header.stamp
+            print('{0} movement time:'.format(r_name))
+            # Movement time
+            robot.movement_time = count_interval_times(mrta.msg.TaskStatus.MOVING,
+                                                       mrta.msg.TaskStatus.ARRIVED,
+                                                       r_status_msgs)
+
+            print('{0} execution time:'.format(r_name))
+            # Execution time
+            robot.execution_time = count_interval_times(mrta.msg.TaskStatus.BEGIN,
+                                                        mrta.msg.TaskStatus.SUCCESS,
+                                                        r_status_msgs)
+
+            print('{0} waiting time:'.format(r_name))
+            # Waiting time
+            robot.waiting_time = count_interval_times(mrta.msg.TaskStatus.ARRIVED,
+                                                      mrta.msg.TaskStatus.BEGIN,
+                                                      r_status_msgs)
+
+            print('{0} delay time:'.format(r_name))
+            # Delay time
+            robot.delay_time = count_interval_times(mrta.msg.TaskStatus.PAUSE,
+                                                    mrta.msg.TaskStatus.RESUME,
+                                                    r_status_msgs)
+
+            for status_msg in r_status_msgs:
+
+                if status_msg.status == mrta.msg.TaskStatus.MOVING and robot.exec_phase_begin_stamp is None:
+                    robot.exec_phase_begin_stamp = status_msg.header.stamp
 
                 #if status_msg.status == 'ALL_TASKS_COMPLETE':
-                if status_msg.status == 'SUCCESS':
-                    robot.travel_end_time = status_msg.header.stamp
+                if status_msg.status == mrta.msg.TaskStatus.SUCCESS or status_msg == mrta.msg.TaskStatus.FAILURE:
+                    robot.exec_phase_end_stamp = status_msg.header.stamp
 
-                if status_msg.status == 'PAUSE':
+                if status_msg.status == mrta.msg.TaskStatus.PAUSE:
                     robot.collisions += 1
-                    robot.pause_times[status_msg.task_id] = status_msg.header.stamp
 
-                if status_msg.status == 'RESUME':
-
-                    pause_time = robot.pause_times[status_msg.task_id]
-                    if not pause_time:
-                        print("'RESUME' message has no matching 'PAUSE' message! {0}, task {1}".format(
-                            r_name, status_msg.task_id))
-                        continue
-
-                    resume_time = status_msg.header.stamp
-                    delay_time = resume_time - pause_time
-                    robot.delay_time +=  (delay_time.secs + delay_time.nsecs/1000000000.)
-
-                    # Clear out the most recent pause time for this task
-                    robot.pause_times[status_msg.task_id] = None
-
-            if not robot.travel_end_time or not robot.travel_begin_time:
+            if not robot.exec_phase_begin_stamp or not robot.exec_phase_end_stamp:
+                print("Can not determine beginning or end time of {0}'s exec phase!".format(r_name))
                 continue
 
-            travel_time_diff = robot.travel_end_time - robot.travel_begin_time
-            robot.travel_time = (travel_time_diff.secs + travel_time_diff.nsecs/1000000000.)
-
-            idle_time_diff = exp_msgs['END_EXECUTION'].header.stamp - robot.travel_end_time
+            #idle_time_diff = exp_msgs['END_EXECUTION'].header.stamp - robot.travel_end_time
+            idle_time_diff = exp_end_execution_stamp - robot.exec_phase_end_stamp
             robot.idle_time = (idle_time_diff.secs + idle_time_diff.nsecs/1000000000.)
-            print "{0} idle time is {1}".format(r_name, robot.idle_time)
-            
+
             # Since messages may arrive out of order it's possible for the idle time
             # of the last robot to be negative. Make the minimum time 0 here.
             if robot.idle_time < 0:
-                print "{0} idle time is negative... ({1})".format(r_name, robot.idle_time)
                 robot.idle_time = 0
 
-            total_idle_time += robot.idle_time
-            total_collisions += robot.collisions
-            total_delay_time += robot.delay_time
+            print("{0} collisions: {1}".format(r_name, robot.collisions))
 
-        row_fields.append(total_idle_time)  # 'TOTAL_IDLE_TIME'
-        row_fields.append(total_delay_time) # 'TOTAL_DELAY_TIME'
-        row_fields.append(total_distance)   # 'TOTAL_DISTANCE'
-        row_fields.append(total_collisions) # 'TOTAL_COLLISIONS'
+            total_movement_time += robot.movement_time
+            total_execution_time += robot.execution_time
+            total_waiting_time += robot.waiting_time
+            total_idle_time += robot.idle_time
+            total_delay_time += robot.delay_time
+            total_distance += robot.distance
+            total_collisions += robot.collisions
+
+        print("total_collisions: {0}".format(total_collisions))
+
+        row_fields['TOTAL_MOVEMENT_TIME'] = total_movement_time   # 'TOTAL_MOVEMENT_TIME'
+        row_fields['TOTAL_EXECUTION_TIME'] = total_execution_time # 'TOTAL_EXECUTION_TIME'
+        row_fields['TOTAL_WAITING_TIME'] = total_waiting_time     # 'TOTAL_WAITING_TIME'
+        row_fields['TOTAL_IDLE_TIME'] = total_idle_time           # 'TOTAL_IDLE_TIME'
+        row_fields['TOTAL_DELAY_TIME'] = total_delay_time         # 'TOTAL_DELAY_TIME'
+        row_fields['TOTAL_DISTANCE'] = total_distance             # 'TOTAL_DISTANCE'
+        row_fields['TOTAL_COLLISIONS'] = total_collisions         # 'TOTAL_COLLISIONS'
+
+        # Deliberation metrics
+        announce_msg_bytes = bid_msg_bytes = award_msg_bytes = 0
 
         # Number of announcement messages
-        num_announcements = len(run_msgs['/tasks/announce'])
-        row_fields.append(num_announcements) # 'ANNOUNCEMENTS'
+        num_announce_msgs = len(run_msgs['/tasks/announce'])
+        row_fields['NUM_ANNOUNCE_MSGS'] = num_announce_msgs       # 'NUM_ANNOUNCE_MSGS'
 
-        # Number of tasks announced in all announcements
         num_ann_tasks = 0
-        alloc_msgs_bytes = 0
         for ann_msg in run_msgs['/tasks/announce']:
             num_ann_tasks += len(ann_msg.tasks)
-            alloc_msgs_bytes += sys.getsizeof(ann_msg)
+            announce_msg_bytes = sys.getsizeof(ann_msg)
 
-        row_fields.append(num_ann_tasks) # 'ANNOUNCE_MSG_TASKS'
+        row_fields['NUM_ANNOUNCE_TASKS'] = num_ann_tasks          # 'NUM_ANNOUNCE_TASKS'
 
         num_bid_msgs = 0
         for bid_msg in run_msgs['/tasks/bid']:
             num_bid_msgs += 1
-            alloc_msgs_bytes += sys.getsizeof(bid_msg)
+            bid_msg_bytes = sys.getsizeof(bid_msg)
 
-        row_fields.append(num_bid_msgs) # 'BID_MSGS'
+        row_fields['NUM_BID_MSGS'] = num_bid_msgs                 # 'NUM_BID_MSGS'
 
         for award_msg in run_msgs['/tasks/award']:
-            alloc_msgs_bytes += sys.getsizeof(award_msg)
+            award_msg_bytes = sys.getsizeof(award_msg)
 
-        row_fields.append(alloc_msgs_bytes) # 'ALLOC_MSGS_BYTES'
+        alloc_msg_bytes = announce_msg_bytes + bid_msg_bytes + award_msg_bytes
 
-        for r_name in ROBOT_NAMES:
+        row_fields['ALLOC_MSG_BYTES'] = alloc_msg_bytes          # 'ALLOC_MSGS_BYTES'
+
+#        for r_name in ROBOT_NAMES:
+#            robot = robots[r_name]
+
+        for i in range(1,4):
+            r_name = 'robot_{0}'.format(i)
             robot = robots[r_name]
 
-            row_fields.append(robot.distance)     # 'ROBOT<n>_DISTANCE'
-            row_fields.append(robot.travel_time)  # 'ROBOT<n>_TRAVEL_TIME'
-            row_fields.append(robot.idle_time)   # 'ROBOT<n>_IDLE_TIME'
-            row_fields.append(robot.delay_time)     # 'ROBOT<n>_DELAY_TIME'
+            row_fields['ROBOT{0}_DISTANCE'.format(i)] = robot.distance           # 'ROBOT<n>_DISTANCE'
+            row_fields['ROBOT{0}_MOVEMENT_TIME'.format(i)] = robot.movement_time # 'ROBOT<n>_MOVEMENT_TIME'
+            row_fields['ROBOT{0}_WAITING_TIME'.format(i)] = robot.waiting_time   # 'ROBOT<n>_WAITING_TIME'
+            row_fields['ROBOT{0}_IDLE_TIME'.format(i)] = robot.idle_time         # 'ROBOT<n>_IDLE_TIME'
+            row_fields['ROBOT{0}_DELAY_TIME'.format(i)] = robot.delay_time       # 'ROBOT<n>_DELAY_TIME'
 
-
-        csv_file.writerow(row_fields)
+        csv_file.writerow([ row_fields[fn] for fn in field_names ])
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser(description='Extract summary statistics for the runs recorded in given bag files.')
+
+    parser.add_argument('bag_file',
+                        nargs='+',
+                        help='Path to the bag file(s) of the experiment(s) to plot.')
+
+    parser.add_argument('-o', '--output',
+                        default=DEFAULT_CSV_FILENAME,
+                        help="Name of the output .csv file")
+
+    args = parser.parse_args()
+
+    bag_files = args.bag_file
+    output = args.output
+
+#    pp.pprint(bag_files)
+
+    # Make one, flat list of paths to log files
+    bag_paths = []
+
+    for path_arg in bag_files:
+        bag_paths.extend(glob.glob(path_arg))
+
+#    print("bag paths: ")
+#    pp.pprint(bag_paths)
+
+    parse_stats(bag_paths, output)
