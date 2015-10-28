32455bc629f7f21a53fd992a7ad928426061a207
Eric Schneider <eric.schneider@liverpool.ac.uk>
2015-10-28 17:59:08 +0000

    Hopefully the last bit of DWA parameter tuning (increased the goal bias).
    Changed the location of task 1 to prevent some hallway collision headaches.

---
+++ scripts/launch_singlemaster_chadwick.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+#$ -cwd
+#$ -j y
+#$ -pe smp 16
+#$ -l exclusive
+#$ -t 1-6
+#$ -V
+#$ -M eric.schneider@liverpool.ac.uk
+#$ -m abe
+
+source /home/esch/.bashrc
+
+export ROS_OS_OVERRIDE=rhel
+
+RUN_COUNT=5
+SCRIPT_DIR=~/GIT/mrta/scripts
+
+#for map in brooklyn # smartlab
+for map in smartlab
+do
+    #for task_file in brooklyn_tasks_A.txt brooklyn_tasks_C.txt brooklyn_tasks_E.txt
+#    for task_file in tasks_A_dynamic.txt
+    for task_file in MR-CT-DA-scenario1.yaml MR-CT-DA-scenario2.yaml
+    do
+	for mechanism in OSI SSI PSI RR
+	do
+	    for start_config in distributed clustered
+	    do
+		for run in `seq 1 ${RUN_COUNT}`
+		do
+		    $SCRIPT_DIR/launch_experiment_singlemaster.py -ng ${mechanism} ${map} ${start_config} ${task_file}
+
+		done # end "run"
+
+	    done # end "start_config"
+
+	done # end "mechanism"
+
+    done # end "task_file"
+
+done # end "map"
