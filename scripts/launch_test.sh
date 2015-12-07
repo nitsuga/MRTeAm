cd6cbf717ff9c911390e68f74c051e19c225a71e
Eric Schneider <eric.schneider@liverpool.ac.uk>
2015-12-07 12:36:57 +0000

    Added multi-master launch scripts for test runs.

---
+++ scripts/launch_test.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#$ -cwd
+#$ -j y
+#$ -pe smp 16
+#$ -l exclusive
+#$ -t 1-6
+#$ -V
+
+#source /home/esch/.bashrc
+
+#export ROS_OS_OVERRIDE=rhel
+
+RUN_COUNT=30
+SCRIPT_DIR=~/GIT/mrta/scripts
+
+for map in smartlab
+do
+    for task_file in three-corners.yaml
+    do
+	for mechanism in RR
+	do
+	    for start_config in clustered distributed
+	    do
+		for run in `seq 1 ${RUN_COUNT}`
+		do
+		    $SCRIPT_DIR/launch_experiment.py -ng ${mechanism} ${map} ${start_config} ${task_file}
+
+		done # end "run"
+
+	    done # end "start_config"
+
+	done # end "mechanism"
+
+    done # end "task_file"
+
+done # end "map"
