ff402170c2473cb756b675333977fa9f0bc0f391
Eric Schneider <eric.schneider@liverpool.ac.uk>
2016-02-25 03:04:18 +0000

    Simulation params matches Turtlebot more closely

--- scripts/launch_chadwick.sh
+++ scripts/launch_chadwick.sh
@@ -3,24 +3,26 @@
 #$ -j y
 #$ -pe smp 16
 #$ -l exclusive
-#$ -t 1-6
+#$ -t 1-10
 #$ -V
+#$ -M eric.schneider@liverpool.ac.uk
+#$ -m abe
 
-#source /home/esch/.bashrc
+source /home/esch/.bashrc
 
-#export ROS_OS_OVERRIDE=rhel
+export ROS_OS_OVERRIDE=rhel
 
-RUN_COUNT=15
+RUN_COUNT=3
 SCRIPT_DIR=~/GIT/mrta/scripts
 
+#for map in brooklyn # smartlab
 for map in smartlab
 do
-#    for task_file in MR-IT-DA-scenario1.yaml MR-CT-DA-scenario1.yaml SR-IT-DA-scenario1.yaml SR-CT-DA-scenario1.yaml
     for task_file in MR-IT-DA-scenario1.yaml MR-CT-DA-scenario1.yaml SR-IT-DA-scenario1.yaml SR-CT-DA-scenario1.yaml MR-IT-DA-scenario2.yaml MR-CT-DA-scenario2.yaml SR-IT-DA-scenario2.yaml SR-CT-DA-scenario2.yaml
     do
 	for mechanism in OSI SSI PSI RR
 	do
-	    for start_config in clustered distributed
+	    for start_config in distributed clustered
 	    do
 		for run in `seq 1 ${RUN_COUNT}`
 		do
