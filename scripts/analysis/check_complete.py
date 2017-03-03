4bdf252066219321077a517fa4d6041cafaaa929
Eric Schneider <eric.schneider@kcl.ac.uk>
2017-03-03 17:02:53 +0000

    No comment.

--- scripts/analysis/check_complete.py
+++ scripts/analysis/check_complete.py
@@ -16,12 +16,15 @@ import yaml
 
 import mrta.msg
 
-def check_complete(bag_paths):
 
+def check_complete(bag_paths):
     dt_re = re.compile('(.*)\.bag')
 
-    for i,bag_path in enumerate(bag_paths):
-#        print("Reading {0}".format(bag_path))
+    num_failed = 0
+    num_succeeded = 0
+
+    for i, bag_path in enumerate(bag_paths):
+        # print("Reading {0}".format(bag_path))
 
         bag = None
         try:
@@ -33,11 +36,11 @@ def check_complete(bag_paths):
         bag_filename = os.path.basename(bag_path)
         bag_dirname = os.path.dirname(bag_path)
 
-        # Create the 'failed' directory if it doesn't exist
-        failed_dirname = os.path.join(bag_dirname, 'FAILED')
-        if not os.path.isdir(failed_dirname):
-            print('...Creating {0}'.format(failed_dirname))
-            os.mkdir(failed_dirname)
+        # # Create the 'failed' directory if it doesn't exist
+        # failed_dirname = os.path.join(bag_dirname, 'FAILED')
+        # if not os.path.isdir(failed_dirname):
+        #     print('...Creating {0}'.format(failed_dirname))
+        #     os.mkdir(failed_dirname)
 
         (map, start_config, mechanism, task_file, remainder) = bag_filename.split('__')
 
@@ -47,25 +50,28 @@ def check_complete(bag_paths):
 
         run_msgs = defaultdict(list)
 
-        for topic,msg,msg_time in bag.read_messages():
+        for topic, msg, msg_time in bag.read_messages():
             run_msgs[topic].append(msg)
 
         bag.close()
 
         exp_msgs = {}
         for msg in run_msgs['/experiment']:
-#            print msg
+            # print msg
             exp_msgs[msg.event] = msg
 
-
-        print('{0}-{1} ({2}):'.format(start_config, mechanism, dt_string)),
+        print('{0} {1} ({2}):'.format(task_file, mechanism, dt_string)),
 
         if mrta.msg.ExperimentEvent.END_EXPERIMENT in exp_msgs:
             print('  COMPLETE!')
+            num_succeeded += 1
         else:
             print('  FAILED!')
+            num_failed += 1
             # move the file to the 'failed' directory
-            shutil.move(bag_path, os.path.join(failed_dirname, bag_filename))
+            # shutil.move(bag_path, os.path.join(failed_dirname, bag_filename))
+
+    print("Total: {0}, (succeeded: {1}, failed: {2})".format(num_failed+num_succeeded, num_succeeded, num_failed))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Check if the experiments for a given set of bag paths actually completed.')
@@ -78,7 +84,7 @@ if __name__ == "__main__":
 
     bag_files = args.bag_file
 
-#    pp.pprint(bag_files)
+    # pp.pprint(bag_files)
 
     # Make one, flat list of paths to log files
     bag_paths = []
@@ -86,7 +92,7 @@ if __name__ == "__main__":
     for path_arg in bag_files:
         bag_paths.extend(glob.glob(path_arg))
 
-#    print("bag paths: ")
-#    pp.pprint(bag_paths)
+    # print("bag paths: ")
+    # pp.pprint(bag_paths)
 
     check_complete(bag_paths)
